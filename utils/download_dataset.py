4edca7e7ba3f26641e8b5d3ef94bf10cd2c95e2d
simonic02415 <s344643@studenti.polito.it>
2025-03-18 14:38:44 +0000

    modificato download_dataset.py

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -1,11 +1,11 @@
 import os
 import shutil
 
-with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+with open('dataset/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
     for line in f:
         fn, cls, *_ = line.split('\t')
-        os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+        os.makedirs(f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
 
-        shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+        shutil.copyfile(f'dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
 
-shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
+shutil.rmtree('dataset/tiny-imagenet/tiny-imagenet-200/val/images')
